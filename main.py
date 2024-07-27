9df147891b2412b0278791b4fe8ca7ec0b20d699
fulliam <fordevjob@gmail.com>
2024-07-27 15:17:09 +0300

    Updated messaging logic

--- main.py
+++ main.py
@@ -6,24 +6,31 @@ import json
 app = FastAPI()
 
 class User(BaseModel):
-    user_id: str
+    userId: str
     character: str = "wizard"
     action: str = "idle"
+
+class Message(BaseModel):
+    userId: str
+    content: str
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: Dict[str, WebSocket] = {}
         self.users: Dict[str, User] = {}
+        self.messages: List[Message] = []
 
     async def connect(self, websocket: WebSocket, user: User):
         await websocket.accept()
-        self.active_connections[user.user_id] = websocket
-        self.users[user.user_id] = user
+        self.active_connections[user.userId] = websocket
+        self.users[user.userId] = user
         await self.broadcast_users()
 
-    def disconnect(self, user_id: str):
-        del self.active_connections[user_id]
-        del self.users[user_id]
+    def disconnect(self, userId: str):
+        if userId in self.active_connections:
+            del self.active_connections[userId]
+        if userId in self.users:
+            del self.users[userId]
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -36,23 +43,32 @@ class ConnectionManager:
         user_list = [user.dict() for user in self.users.values()]
         await self.broadcast(json.dumps({"type": "users", "data": user_list}))
 
+    async def broadcast_messages(self):
+        message_list = [message.dict() for message in self.messages]
+        await self.broadcast(json.dumps({"type": "messages", "data": message_list}))
+
 manager = ConnectionManager()
 
-@app.websocket("/ws/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: str):
-    user = User(user_id=user_id)
+@app.websocket("/ws/{userId}")
+async def websocket_endpoint(websocket: WebSocket, userId: str):
+    user = User(userId=userId)
     await manager.connect(websocket, user)
     try:
         while True:
             data = await websocket.receive_text()
             message_data = json.loads(data)
-            if message_data["type"] == "action":
+            if message_data.get("type") == "action":
                 user.character = message_data.get("character", user.character)
                 user.action = message_data.get("action", user.action)
-                manager.users[user_id] = user
+                manager.users[userId] = user
                 await manager.broadcast_users()
+            elif message_data.get("type") == "message":
+                content = message_data.get("content", "")
+                message = Message(userId=userId, content=content)
+                manager.messages.append(message)
+                await manager.broadcast_messages()
     except WebSocketDisconnect:
-        manager.disconnect(user_id)
+        manager.disconnect(userId)
         await manager.broadcast_users()
 
 if __name__ == '__main__':
